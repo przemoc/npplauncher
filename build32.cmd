3be18d08a8039f33d1389e4d87e5cce75f40b9e6
Przemyslaw Pawelczyk <przemoc@gmail.com>
2017-06-13 01:35:56 +0200

    Update build*.cmd to work correctly with new default.
    I no longer use these scripts, but if there are some users of them...

--- build32.cmd
+++ build32.cmd
@@ -2,5 +2,5 @@
 @set PATH=%PATH%;D:\MinGW-W64-%ARCH%\bin
 @set PREFIX=%ARCH%-w64-mingw32-
 %PREFIX%windres -i notepad.rc --input-format=rc -o notepad.res -O coff
-%PREFIX%gcc -Wall -O2 -std=c99 notepad.c notepad.res -o %ARCH%/notepad.exe -mwindows -s
-%PREFIX%gcc -Wall -O2 -std=c99 notepad.c notepad.res -o %ARCH%/wait_for_exit/notepad.exe -mwindows -s -DWAIT_FOR_EXIT=1
+%PREFIX%gcc -Wall -O2 -std=c99 notepad.c notepad.res -o %ARCH%/notepad.exe -mwindows -s -DWAIT_FOR_EXIT=1
+%PREFIX%gcc -Wall -O2 -std=c99 notepad.c notepad.res -o %ARCH%/no_wait_for_exit/notepad.exe -mwindows -s -DWAIT_FOR_EXIT=0
